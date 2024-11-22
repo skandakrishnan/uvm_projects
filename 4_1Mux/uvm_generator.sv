49c82679a421460b0a88e7f643b95fd1a1687dc5
skb_kochi <skandakrishnan96@gmail.com>
2024-11-22 17:59:49 -0500

    Add files via upload

---
+++ 4_1Mux/uvm_generator.sv
@@ -0,0 +1,31 @@
+
+
+
+class generator extends uvm_sequence #(transaction);
+  `uvm_object_utils(generator)
+  
+  transaction t;
+  
+  integer i=5;
+  
+  function new(input string path = "generator");
+    super.new(path);
+  endfunction
+  
+  
+  
+  
+  
+  virtual task body();
+    t = transaction::type_id::create("t");
+    repeat (i) begin
+      start_item(t);
+      assert(t.randomize());
+      `uvm_info("GEN",$sformatf("a: %0d, b: %0d", t.a, t.b) ,UVM_NONE);
+      finish_item(t);
+    end
+  endtask
+  
+  
+  
+endclass
